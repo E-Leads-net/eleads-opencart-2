df258bd98753cdcadbac219c6cdc9fde58a5fc20
Dmitry Plohotnyuk <dmitryplohotnyuk@MacBook-Pro-Dmitry.local>
2026-02-25 17:05:04 +0200

    v0.1.29

--- upload/system/library/eleads/oc_adapter.php
+++ upload/system/library/eleads/oc_adapter.php
@@ -192,6 +192,7 @@ class EleadsOcAdapter {
 			$result[] = array(
 				'product_id' => (int)$product['product_id'],
 				'name' => $product['name'],
+				'model' => isset($product['model']) ? $product['model'] : '',
 				'description' => $product['description'],
 				'meta_description' => $product['meta_description'],
 				'manufacturer_id' => (int)$product['manufacturer_id'],
@@ -211,6 +212,7 @@ class EleadsOcAdapter {
 		return array(
 			'product_id' => (int)$product['product_id'],
 			'name' => $product['name'],
+			'model' => isset($product['model']) ? $product['model'] : '',
 			'description' => $product['description'],
 			'meta_description' => $product['meta_description'],
 			'manufacturer_id' => (int)$product['manufacturer_id'],
